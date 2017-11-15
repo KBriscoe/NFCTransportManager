ff5df54ba2e8e180f7ed07f3f57936ca052c261d
KBriscoe <Kyler.briscoe@gmail.com>
2017-11-15 14:27:15 -0500

    Added Scan Activity

--- app/src/main/java/com/koyomi/nfc_transport_manager/MainActivity.java
+++ app/src/main/java/com/koyomi/nfc_transport_manager/MainActivity.java
@@ -1,43 +1,56 @@
 package com.koyomi.nfc_transport_manager;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.EditText;
+import android.view.View;
+import android.widget.Button;
 
 import java.sql.*;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button loginButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        initialize();
     }
 
+    private void initialize() {
+        loginButton = findViewById(R.id.loginButton);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(MainActivity.this, ScanActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
 
     public void ConnectToDatabase() {
-        try {
+                try {
 
-            // SET CONNECTIONSTRING
-            Class.forName("net.sourceforge.jtds.jdbc.Driver").newInstance();
-            String username = "XXXXXXXXX";
-            String password = "XXXXXX";
-            Connection DbConn = DriverManager.getConnection("jdbc:jtds:sqlserver://192.188.4.83:1433/DATABASE;user=" + username + ";password=" + password);
+                    // SET CONNECTIONSTRING
+                    Class.forName("net.sourceforge.jtds.jdbc.Driver").newInstance();
+                    String username = "XXXXXXXXX";
+                    String password = "XXXXXX";
+                    Connection DbConn = DriverManager.getConnection("jdbc:jtds:sqlserver://192.188.4.83:1433/DATABASE;user=" + username + ";password=" + password);
 
-            Log.w("Connection", "open");
-            Statement stmt = DbConn.createStatement();
-            ResultSet reset = stmt.executeQuery(" select * from users ");
+                    Log.w("Connection", "open");
+                    Statement stmt = DbConn.createStatement();
+                    ResultSet reset = stmt.executeQuery(" select * from users ");
 
 
-            // EditText num = (EditText) findViewById(R.id.displaymessage);
-            // num.setText(reset.getString(1));
+                    // EditText num = (EditText) findViewById(R.id.displaymessage);
+                    // num.setText(reset.getString(1));
 
-            DbConn.close();
+                    DbConn.close();
 
-        } catch (Exception e) {
-            Log.w("Error connection", "" + e.getMessage());
-        }
-    }
+                } catch (Exception e) {
+                    Log.w("Error connection", "" + e.getMessage());
+                }
+            }
 }
---
+++ app/src/main/java/com/koyomi/nfc_transport_manager/ScanActivity.java
@@ -0,0 +1,12 @@
+package com.koyomi.nfc_transport_manager;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ScanActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.scan_layout);
+        }
+    }
