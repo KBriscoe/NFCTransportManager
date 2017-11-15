9fb351616553e0d808228db21e67bf4dea4bb264
Tyler Bratton <tylerbratton96@gmail.com>
2017-11-15 15:36:53 -0500

    Add login/registration code that communicates with server

--- app/src/main/java/com/koyomi/nfc_transport_manager/MainActivity.java
+++ app/src/main/java/com/koyomi/nfc_transport_manager/MainActivity.java
@@ -1,13 +1,100 @@
 package com.koyomi.nfc_transport_manager;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+  EditText passwordField;
+  EditText usernameField;
+  Button loginButton;
+  Button signupButton;
+  Context context = this;
+
+  final static String URL = "http://10.0.2.2/test_android/index.php";
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+
+    usernameField = findViewById(R.id.usernameField);
+    passwordField = findViewById(R.id.passwordField);
+
+    loginButton = findViewById(R.id.loginButton);
+    signupButton = findViewById(R.id.signupButton);
+
+    loginButton.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        makeRequest(usernameField.getText().toString(), passwordField.getText
+            ().toString(), "login");
+      }
+    });
+
+    signupButton.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        makeRequest(usernameField.getText().toString(), passwordField.getText
+            ().toString(), "signup");
+      }
+    });
+
+
+  }
+
+  public void makeRequest(String... args) {
+    final String username = args[0];
+    final String password = args[1];
+    final String login = args[2];
+    RequestQueue queue = Volley.newRequestQueue(context);
+    StringRequest postRequest = new StringRequest(Request.Method.POST, URL,
+        new Response.Listener<String>() {
+          @Override
+          public void onResponse(String response) {
+            try {
+              JSONObject object = new JSONObject(response);
+              Toast.makeText(getApplicationContext(), object.getString
+                  ("message"), Toast.LENGTH_LONG).show();
+            } catch (Exception ex) {
+              System.out.println("woops");
+            }
+          }
+        },
+
+        new Response.ErrorListener() {
+          @Override
+          public void onErrorResponse(VolleyError error) {
+            Toast.makeText(getApplicationContext(), "Unable to retrieve any data from server", Toast.LENGTH_LONG).show();
+          }
+        }
+    ) {
+      @Override
+      protected Map<String, String> getParams() {
+        Map<String, String>  params = new HashMap<>();
+        params.put("username", username);
+        params.put("password", password);
+        params.put("login", login);
+
+        return params;
+      }
+    };
+    queue.add(postRequest);
+  }
 }
