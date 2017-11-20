533563f3ac41f8f221f1ca46fb9c6495d3b9cf77
Tyler Bratton <tylerbratton96@gmail.com>
2017-11-20 11:07:23 -0500

    Lambda functions babyyyyy

--- app/src/main/java/com/koyomi/nfc_transport_manager/MainActivity.java
+++ app/src/main/java/com/koyomi/nfc_transport_manager/MainActivity.java
@@ -4,14 +4,11 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
@@ -44,12 +41,8 @@ public class MainActivity extends AppCompatActivity {
         loginButton = findViewById(R.id.loginButton);
         signupButton = findViewById(R.id.signupButton);
 
-        loginButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                makeRequest(new VolleyCallback(){
-                    @Override
-                    public void onSuccess(JSONObject result) {
+        loginButton.setOnClickListener(view ->
+                loginRequest(result ->  {
                         try {
                             if (result.getInt("status") == 200) {
                                 //Assign correct PassID based on username here
@@ -64,35 +57,25 @@ public class MainActivity extends AppCompatActivity {
                         } catch (Exception ex) {
                             System.out.println(ex.toString());
                         }
-                    }
                 },email.getText().toString(), password.getText
-                        ().toString(), "login");
-            }
-        });
+                        ().toString(), "login"));
 
-        signupButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                makeRequest(new VolleyCallback() {
-                    @Override
-                    public void onSuccess(JSONObject result) {
 
-                    }
-                },email.getText().toString(), password.getText
-                        ().toString(), "signup");
-            }
-        });
+        signupButton.setOnClickListener(view ->
+                signupRequest(result -> {
+                  // Do whatever action is wanted after sign up.
+                  System.out.print("hi");
+                  System.out.print("hi");
+                }, email.getText().toString(), password.getText().toString(),
+                    "signup"));
     }
 
-    public void makeRequest(final VolleyCallback callback, String... args) {
+    public void loginRequest(final VolleyCallback callback, String... args) {
         final String email = args[0];
         final String password = args[1];
-        final String login = args[2];
         RequestQueue queue = Volley.newRequestQueue(context);
         StringRequest postRequest = new StringRequest(Request.Method.POST, URL,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
+                response -> {
                         try {
                             JSONObject object = new JSONObject(response);
                             Toast.makeText(getApplicationContext(), object.getString
@@ -101,28 +84,61 @@ public class MainActivity extends AppCompatActivity {
                         } catch (Exception ex) {
                             System.out.println(ex.toString());
                         }
-                    }
-                },
+                    },
 
-                new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        Toast.makeText(getApplicationContext(), "Unable to retrieve any data from server", Toast.LENGTH_LONG).show();
-                    }
-                }
+                error ->
+                    Toast.makeText(getApplicationContext(), "Unable to "
+                        + " retrieve any data from server", Toast.LENGTH_LONG)
+                        .show()
+
         ) {
             @Override
             protected Map<String, String> getParams() {
                     Map<String, String>  params = new HashMap<>();
                     params.put("email", email);
                     params.put("password", password);
-                    params.put("login", login);
-
                     return params;
             }
         };
         queue.add(postRequest);
     }
+
+  public void signupRequest(final VolleyCallback callback, String... args) {
+    final String email = args[0];
+    final String password = args[1];
+    final String fname = args[2];
+    final String lname = args[3];
+    RequestQueue queue = Volley.newRequestQueue(context);
+    StringRequest postRequest = new StringRequest(Request.Method.POST, URL,
+        response -> {
+          try {
+            JSONObject object = new JSONObject(response);
+            Toast.makeText(getApplicationContext(), object.getString
+                ("message"), Toast.LENGTH_LONG).show();
+            callback.onSuccess(object);
+          } catch (Exception ex) {
+            System.out.println(ex.toString());
+          }
+        },
+
+        error ->
+            Toast.makeText(getApplicationContext(), "Unable to "
+                + " retrieve any data from server", Toast.LENGTH_LONG)
+                .show()
+
+    ) {
+      @Override
+      protected Map<String, String> getParams() {
+        Map<String, String>  params = new HashMap<>();
+        params.put("email", email);
+        params.put("password", password);
+        params.put("fname", fname);
+        params.put("lname", lname);
+        return params;
+      }
+    };
+    queue.add(postRequest);
+  }
 
     public interface VolleyCallback{
         void onSuccess(JSONObject result);
