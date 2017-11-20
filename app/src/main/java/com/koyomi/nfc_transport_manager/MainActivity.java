2fc495c728a4b8654e17d088a8356847979e1a82
KBriscoe <Kyler.briscoe@gmail.com>
2017-11-20 14:10:09 -0500

    Added signup view and adjusted main_view to flip views

--- app/src/main/java/com/koyomi/nfc_transport_manager/MainActivity.java
+++ app/src/main/java/com/koyomi/nfc_transport_manager/MainActivity.java
@@ -5,9 +5,12 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+import android.widget.ViewSwitcher;
+
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
@@ -26,6 +29,8 @@ public class MainActivity extends AppCompatActivity {
     EditText email;
     Button loginButton;
     Button signupButton;
+    Button confirmSignupButton;
+    Button backButton;
     Context context = this;
 
     final static String URL = "http://34.239.52.7/index.php";
@@ -37,51 +42,64 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initialize() {
+        ViewSwitcher viewSwitcher = findViewById(R.id.viewSwitcher);
+        View loginView = findViewById(R.id.loginView);
+        View signupView = findViewById(R.id.signupView);
+
         email = findViewById(R.id.emailField);
         email = findViewById(R.id.emailField);
         password = findViewById(R.id.passwordField);
 
         loginButton = findViewById(R.id.loginButton);
         signupButton = findViewById(R.id.signupButton);
+        confirmSignupButton = findViewById(R.id.createButton);
+        backButton = findViewById(R.id.backButton);
 
-        loginButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                makeRequest(new VolleyCallback(){
-                    @Override
-                    public void onSuccess(JSONObject result) {
-                        try {
-                            if (result.getInt("status") == 200) {
-                                //Assign correct PassID based on username here
-                                passID = email.getText().toString();
-                                int IDLength = passID.length();
-                                String sendingID = passID;
-                                Intent intent = new Intent(MainActivity.this, ScanActivity.class);
-                                intent.putExtra("ID", sendingID);
-                                intent.putExtra("ID Length", IDLength);
-                                startActivity(intent);
-                            }
-                        } catch (Exception ex) {
-                            System.out.println(ex.toString());
+        loginButton.setOnClickListener(view -> {
+                makeRequest(result -> {
+                    try {
+                        if (result.getInt("status") == 200) {
+                            //Populate passID from database
+                            passID = email.getText().toString();
+                            int IDLength = passID.length();
+                            String sendingID = passID;
+                            Intent intent = new Intent(MainActivity.this, ScanActivity.class);
+                            intent.putExtra("ID", sendingID);
+                            intent.putExtra("ID Length", IDLength);
+                            startActivity(intent);
                         }
+                    } catch (Exception ex) {
+                        System.out.println(ex.toString());
                     }
-                },email.getText().toString(), password.getText
+
+                }, email.getText().toString(), password.getText
                         ().toString(), "login");
-            }
         });
 
-        signupButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                makeRequest(new VolleyCallback() {
-                    @Override
-                    public void onSuccess(JSONObject result) {
+        signupButton.setOnClickListener(View -> {
+                if (viewSwitcher.getCurrentView() != loginView){
+                    viewSwitcher.showPrevious();
+                }else if(viewSwitcher.getCurrentView() != signupView){
+                    viewSwitcher.showNext();
+                }
+            }
+        );
 
+        confirmSignupButton.setOnClickListener(View -> {
+                    //Check validity of info
+
+                    //SQL add profile info from text boxes.
+                }
+        );
+
+        backButton.setOnClickListener(View -> {
+                    if (viewSwitcher.getCurrentView() != loginView){
+                        viewSwitcher.showPrevious();
+                    }else if(viewSwitcher.getCurrentView() != signupView){
+                        viewSwitcher.showNext();
                     }
-                },email.getText().toString(), password.getText
-                        ().toString(), "signup");
-            }
-        });
+                }
+        );
     }
 
     public void makeRequest(final VolleyCallback callback, String... args) {
