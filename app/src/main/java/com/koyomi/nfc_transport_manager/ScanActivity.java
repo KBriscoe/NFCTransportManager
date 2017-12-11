eff85839ef3991b4561ea9a264ea442f05f7ee05
KBriscoe <Kyler.briscoe@gmail.com>
2017-12-12 06:30:04 +0900

    Updated ScanActivity

--- app/src/main/java/com/koyomi/nfc_transport_manager/ScanActivity.java
+++ app/src/main/java/com/koyomi/nfc_transport_manager/ScanActivity.java
@@ -1,33 +1,89 @@
 package com.koyomi.nfc_transport_manager;
 
 import android.app.Activity;
+import android.net.Uri;
 import android.nfc.NdefRecord;
-import android.nfc.tech.MifareUltralight;
+import android.nfc.NfcAdapter;
+import android.nfc.NfcEvent;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.util.Log;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.nio.charset.Charset;
 
 public class ScanActivity extends Activity {
 
-    private static final String TAG = MifareUltralight.class.getSimpleName();
-    private TextView passText;
-    private String sentID;
+    // List of URIs to provide to Android Beam
+    private Uri[] mFileUris = new Uri[10];
+    // Instance that returns available files from this app
+    private FileUriCallback mFileUriCallback;
+
+    private String filename;
+    // Flag to indicate that Android Beam is available
+    boolean mAndroidBeamAvailable = false;
+    NfcAdapter mNfcAdapter;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.scan_activity);
-        //Setting title ID
-        //Bundle extras = getIntent().getExtras();
-        //sentID = extras.getString("ID");
-        //int len = extras.getInt("ID Length");
-        passText = (TextView)findViewById(R.id.passText);
-        //passText.setText(sentID.toCharArray(), 0, len);
-
 
         NdefRecord mimeRecord = new NdefRecord(
                 NdefRecord.TNF_MIME_MEDIA,
                 "application/vnd.com.example.android.beam".getBytes(Charset.forName("US-ASCII")),
                 new byte[0], "Beam me up, Android!".getBytes(Charset.forName("US-ASCII")));
+
+        Bundle extras = getIntent().getExtras();
+        String id = extras.getString("ID");
+        filename = id + ".tsc";
+
+        mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
+        mFileUriCallback = new FileUriCallback();
+        // Set the dynamic callback for URI requests.
+        mNfcAdapter.setBeamPushUrisCallback(mFileUriCallback,this);
+
+    }
+
+    class FileUriCallback implements NfcAdapter.CreateBeamUrisCallback {
+        // List of URIs to provide to Android Beam
+        private Uri[] mFileUris = new Uri[10];
+        public FileUriCallback() {
+        }
+        /**
+         * Create content URIs as needed to share with another device
+         */
+        @Override
+        public Uri[] createBeamUris(NfcEvent event) {
+            try {
+                // catches IOException below
+                final String SEPARATOR = "::";
+                FileOutputStream fOut = openFileOutput(filename, MODE_WORLD_READABLE);
+                OutputStreamWriter osw = new OutputStreamWriter(fOut);
+
+                // Write to the file
+                osw.write(SEPARATOR);
+                osw.flush();
+                osw.close();
+
+            } catch (IOException ioe)
+            {ioe.printStackTrace();}
+            String transferFile = filename;
+            File extDir = getExternalFilesDir(null);
+            File requestFile = new File(extDir, transferFile);
+            requestFile.setReadable(true, false);
+            // Get a URI for the File and add it to the list of URIs
+            Uri fileUri = Uri.fromFile(requestFile);
+            if (fileUri != null) {
+                mFileUris[0] = fileUri;
+            } else {
+                Log.e("My Activity", "No File URI available for file.");
+            }
+            return mFileUris;
+        }
     }
 }
+
+
